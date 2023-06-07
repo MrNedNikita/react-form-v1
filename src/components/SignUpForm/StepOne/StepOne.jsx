e8476fa573e0293c0232351eed94f9c5d554d743
MrNedNick <mrnediala@gmail.com>
2023-06-07 21:58:07 +0200

    Add style for step one

--- src/components/SignUpForm/StepOne/StepOne.jsx
+++ src/components/SignUpForm/StepOne/StepOne.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import s from './StepOne.module.css';
 import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
 const StepOne = () => {
   return (
-    <div className="step1">
-      <h4>Start your free 14 days trial - no cancellation required!</h4>
-      <form>
-        <input type="text" placeholder="Email" />
-        <input type="number" placeholder="Number" />
-        <Button className={s.button} variant="contained">Register</Button>
-      </form>
-    </div>
+    <form>
+      <div className={s.container}>
+        <span className={s.text}>Start your free 14 days trial - no cancellation required!</span>
+        <TextField id="standard-basic" label="Email Address" variant="standard" />
+        <span className={s.text}>Find out how to optimize your website with XOVI:</span>
+        <TextField id="standard-basic" label="Telephone Number" variant="standard" />
+        <span className={s.text}>XOVI GmbH will process your provided data to process your request in accordance to its Privacy Policy.</span>
+        <Button className={s.button} variant="contained">Step 2 &gt;</Button>
+      </div>
+    </form>
   );
 };
 
